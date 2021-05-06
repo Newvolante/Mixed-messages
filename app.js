7c852fea5f5c17c4c1ed77b7aa5cdd690d4dac59
Newvolante <60291015+Newvolante@users.noreply.github.com>
2021-05-06 12:06:32 +0200

    added quotes in a separate js file, and linked it in app.js

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+let messages = require('./quotes');
+
 function outputMessage() {
     
 }
---
+++ quotes.js
@@ -0,0 +1,3 @@
+let messages = ["Dura lex, sed lex", "Per Aspera, ad astra", "Comunque vada sarà un successo"];
+
+module.exports = messages;
